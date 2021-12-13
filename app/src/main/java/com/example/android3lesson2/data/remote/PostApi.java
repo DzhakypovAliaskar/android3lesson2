185904b988142c2d6a71639bf58c9ed0eb5a2b33
DzhakypovAliaskar <dzhakypov007@gmail.com>
2021-12-13 19:05:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android3lesson2/data/remote/PostApi.java
@@ -0,0 +1,20 @@
+package com.example.android3lesson2.data.remote;
+
+import com.example.android3lesson2.data.models.Post;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface PostApi {
+
+    @GET("/posts")
+    Call<List<Post>> getPosts();
+
+    @POST("/posts")
+    Call<Post> createPost(
+            @Body Post post
+    );
+}
---
+++ app/src/main/java/com/example/android3lesson2/ui/posts/PostsAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android3lesson2.ui.posts;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.android3lesson2.data.models.Post;
+import com.example.android3lesson2.databinding.ItemPostsBinding;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostViewHolder>{
+
+    private List<Post> posts = new ArrayList<>();
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemPostsBinding binding = ItemPostsBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        );
+        return new PostViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+holder.OnBind(posts.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder{
+
+        private ItemPostsBinding binding;
+
+        public PostViewHolder(@NonNull ItemPostsBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void OnBind(Post post) {
+            binding.tvTitle.setText(post.getTitle());
+            binding.tvContent.setText(post.getTitle());
+            binding.tvUserId.setText(String.valueOf(post.getUserId()));
+        }
+    }
+}
